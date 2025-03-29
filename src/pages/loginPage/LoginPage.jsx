97872702c643ac60189a67fa68f9cbc077ad2ca5
Michael Farrah Girgis <micfar85@gmail.com>
2025-03-29 13:23:22 +0100

    save for laptop

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -4,23 +4,25 @@ import NavBar from "../../components/navBar/NavBar.jsx";
 import {LanguageContext} from "../../context/LanguageContext.jsx";
 import languageContent from "../../content/content.json";
 import "./LoginPage.css";
-import { useForm } from "react-hook-form";
+import {useForm} from "react-hook-form";
 
 
 function LoginPage() {
 
-  const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-   const onSubmit = (data) => {
-        console.log(data);
-   }
-    const {language} = useContext(LanguageContext);
-    const {title, emailTitle, passwordTitle} = languageContent[language].loginpage;
+
     const {
         register,
         handleSubmit,
-        formState: { errors },
+        formState: {errors},
     } = useForm();
+
+    const {language} = useContext(LanguageContext);
+
+    const {title, emailTitle, passwordTitle} = languageContent[language].loginpage;
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
 
 
     return (
@@ -29,17 +31,15 @@ function LoginPage() {
             <h1>{title}</h1>
             <p>{emailTitle}</p>
             <form onSubmit={handleSubmit(onSubmit)}>
-            <InputField
-            inputType= "email"
-            inputId="email"
-            inputName="email"
-            placeholder={emailTitle.toLowerCase()}
-            register={register}
-            validationRules={{required: "Email is required"}}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
-            <p>{passwordTitle}</p>
+                <InputField
+                    inputType="email"
+                    inputId="email"
+                    inputName="email"
+                    placeholder={emailTitle.toLowerCase()}
+                    register={register}
+                    validationRules={{required: "Email is required"}}
+                />
+                <p>{passwordTitle}</p>
 
                 <InputField
                     inputType="password"
@@ -47,16 +47,20 @@ function LoginPage() {
                     inputName="password"
                     placeholder={passwordTitle.toLowerCase()}
                     register={register}
-                    validationRules={{required: "Password is required"}}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    validationRules={{
+                        required: "Password is required",
+                        minLength: {
+                            value: 4,
+                            message: "Password must have at least 4 characters"
+                        }
+                    }}
                 />
 
-            {errors.email && <p>{errors.email.message}</p>}
-            {errors.password && <p>{errors.password.message}</p>}
+                {errors.email && <p>{errors.email.message}</p>}
+                {errors.password && <p>{errors.password.message}</p>}
 
-            <button type="submit">Submit</button>
-        </form>
+                <button type="submit">Submit</button>
+            </form>
         </>
     );
 }
